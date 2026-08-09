0ed5cb352f896519f4a3d01e1ba3780a0734ddb4
Nova Nurachman <nvnrchmn@logikraf.id>
2026-08-10 05:42:03 +0700

    ui: percantik blog show (author, reading time, share, related posts)

--- app/Livewire/BlogShow.php
+++ app/Livewire/BlogShow.php
@@ -10,12 +10,27 @@ use Livewire\Component;
 class BlogShow extends Component
 {
     public Post $post;
+    public $related = [];
 
     public function mount(Post $post): void
     {
         if (!$post->is_published) {
             abort(404);
         }
+        $this->post = $post->load('author');
+        $this->related = Post::query()
+            ->where('is_published', true)
+            ->where('id', '!=', $post->id)
+            ->where('author_id', $post->author_id)
+            ->orderByDesc('published_at')
+            ->limit(3)
+            ->get();
+    }
+
+    public function readingTime(): int
+    {
+        $words = str_word_count(strip_tags($this->post->body));
+        return max(1, (int) ceil($words / 200));
     }
 
     public function render()
