f5a35c9cb3b4ee5a02686df74be2001022345625
nvnrchmn <nvnrchmn@users.noreply.github.com>
2026-09-21 06:10:12 +0700

    fix(seo): add lastmod + updated_at to sitemap.xml, fix portfolio category filter
    - sitemap.xml: add <lastmod> with updated_at per dynamic route (/portfolio, /blog)
    - static routes: empty lastmod (neutral)
    - portfolio/blog slug queries: use Select(slug, updated_at) instead of Pluck
    
    Refs: .hermesrules line 68 (BELUM: sitemap tanpa <lastmod>)

--- logikraf-v2/internal/delivery/handler/sitemap.go
+++ logikraf-v2/internal/delivery/handler/sitemap.go
@@ -17,22 +17,29 @@ func GetSitemap(c fiber.Ctx) error {
 	b.WriteString(`<?xml version="1.0" encoding="UTF-8"?>` + "\n")
 	b.WriteString(`<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">` + "\n")
 
-	write := func(path string) {
-		b.WriteString("  <url><loc>" + siteBaseURL + path + "</loc></url>\n")
+	write := func(path, lastmod string) {
+		if lastmod != "" {
+			b.WriteString("  <url><loc>" + siteBaseURL + path + "</loc><lastmod>" + lastmod + "</lastmod></url>\n")
+		} else {
+			b.WriteString("  <url><loc>" + siteBaseURL + path + "</loc></url>\n")
+		}
 	}
 	for _, p := range staticPaths {
-		write(p)
+		write(p, "")
 	}
 
-	var slugs []string
-	model.DB.Model(&model.Portfolio{}).Where("is_published = ?", true).Pluck("slug", &slugs)
-	for _, s := range slugs {
-		write("/portfolio/" + s)
+	var slugDates []struct {
+		Slug      string
+		UpdatedAt string
 	}
-	slugs = nil
-	model.DB.Model(&model.Post{}).Where("is_published = ?", true).Pluck("slug", &slugs)
-	for _, s := range slugs {
-		write("/blog/" + s)
+	model.DB.Model(&model.Portfolio{}).Where("is_published = ?", true).Select("slug, updated_at").Scan(&slugDates)
+	for _, s := range slugDates {
+		write("/portfolio/"+s.Slug, s.UpdatedAt[:10])
+	}
+	slugDates = nil
+	model.DB.Model(&model.Post{}).Where("is_published = ?", true).Select("slug, updated_at").Scan(&slugDates)
+	for _, s := range slugDates {
+		write("/blog/"+s.Slug, s.UpdatedAt[:10])
 	}
 
 	b.WriteString("</urlset>")
